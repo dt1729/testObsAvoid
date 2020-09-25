5e9633101c585ec30d5dd4ef66c1f97d2cde870b
dt1729 <divyatiwari1112@gmail.com>
2020-09-25 14:57:40 +0530

    first_commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/rosmachine/ROSworkspaces/testObsAvoid/src
+# Build directory: /home/rosmachine/ROSworkspaces/testObsAvoid/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("obstacle-avoidance-turtlebot")
---
+++ build/obstacle-avoidance-turtlebot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rosmachine/ROSworkspaces/testObsAvoid/src/obstacle-avoidance-turtlebot
+# Build directory: /home/rosmachine/ROSworkspaces/testObsAvoid/build/obstacle-avoidance-turtlebot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
